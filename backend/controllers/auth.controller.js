b9affb4eb833d9feaa899acd408dfa1da7d41d3f
suraj171299 <surajpadalkar1999@gmail.com>
2024-10-04 08:38:02 +0530

    Implemented reset password link through email

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import { User } from "../models/user.model.js";
 import crypto from  'crypto'
 import bcrypt from 'bcryptjs';
 import { generateTokenandSetCookie } from "../utils/generateTokenandSetCookie.js";
-import { sendVerificationEmail, sendWelcomeEmail,sendPasswordResetEmail } from "../mailtrap/email.js";
+import { sendVerificationEmail, sendWelcomeEmail,sendPasswordResetEmail, sendResetSuccessEmail } from "../mailtrap/email.js";
 
 export const signup = async (req, res) => {
     const { email, password, username } = req.body
@@ -169,4 +169,45 @@ export const forgotPassword = async (req, res) => {
             message: error.message
         })
     }
+}
+
+export const resetPassword = async (req, res) => {
+    try {
+        const { token } = req.params
+        const { password } = req.body
+
+        const user = await User.findOne({
+            resetPasswordToken: token,
+            resetPasswordExpiresAt: { $gt: Date.now() }
+        })
+
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid or expired token"
+            })
+        }
+
+        // update password
+        const hashedPassword = await bcrypt.hash(password, 10)
+        
+        user.password = hashedPassword
+        user.resetPasswordToken = undefined
+        user.resetPasswordExpiresAt = undefined
+
+        await user.save()
+
+        await sendResetSuccessEmail(user.email)
+
+        res.status(200).json({
+            success: true,
+            message: "Password reset successfully"
+        })
+    } catch (error) {
+        console.error('Error in reset password', error)
+        res.status(400).json({
+            success: false,
+            message: error.message
+        })
+    } 
 }
--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -1,4 +1,4 @@
-import { PASSWORD_RESET_REQUEST_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
+import { PASSWORD_RESET_REQUEST_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
 import { mailtrapClient, sender } from "./mailtrap.config.js"
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -52,8 +52,26 @@ export const sendPasswordResetEmail = async (email, resetURL) => {
             category: "Password Reset"
         })
     } catch (error) {
-        console.error('Error sending password reser email', error)
+        console.error('Error sending password reset email', error)
 
-        throw new Error(`Error sending password reser email: ${error}`)
+        throw new Error(`Error sending password reset email: ${error}`)
+    }
+}
+
+export const sendResetSuccessEmail = async (email) => {
+    const recipient = [{ email }]
+    
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "Password Reset Successful",
+            html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+            category: "Password Reset" 
+        })
+    } catch (error) {
+        console.error('Error sending password success email', error)
+
+        throw new Error(`Error sending password success email: ${error}`)
     }
 }
